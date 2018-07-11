5d152d3274a505eecd54e9e3850bc46576a9faf2
Nat Knight <nathaniel.ep@gmail.com>
2018-07-10 21:28:23 -0700

    Update test suite to use tinytest

--- tests/test_serde.js
+++ tests/test_serde.js
@@ -1,8 +1,3 @@
-import test from 'ava';
-import SerDe from "../app";
-import btoa from "btoa";
-import atob from "atob";
-
 
 const EXAMPLE_ITEMS = [
     {id: "1", content: "First", done: true},
@@ -10,22 +5,48 @@ const EXAMPLE_ITEMS = [
     {id: "3", content: "Third", done: false},
 ];
 
-test("serializing a ChecklistItem should succeed", t => {
-    t.plan(0); // just checking for exceptions
-    SerDe.serialize(EXAMLE_ITEMS);
-});
-
-test("round-tripping items should be idempotent", t => {
-    t.plan(1);
-    t.deepEqual(
-        EXAMPLE_ITEMS,
-        SerDe.deserialize(SerDe.serialize(EXAMPLE_ITEMS)),
-    );
-});
-
-test("deserializing invalid input throws an exception", t => {
-    t.plan(1);
-    let input = [{these: "are", the: "wrong keys", and: "values"}];
-    let serialized = btoa(JSON.stringify(input));
-    t.throws(() => SerDe.deserialize(serialized));
+tests({
+    "serialize doesn't throw an exception": function() {
+        SerDe.serialize(EXAMPLE_ITEMS);
+    },
+    "serialize-deserialize is idempotent": function() {
+        let serialized = SerDe.serialize(EXAMPLE_ITEMS);
+        let round_tripped = SerDe.deserialize(serialized);
+        eq(round_tripped, EXAMPLE_ITEMS);
+    },
+    "deserialize-serialize is idempotent": function() {
+        let serialized = SerDe.serialize(EXAMPLE_ITEMS);
+        let deserialized = SerDe.deserialize(serialized);
+        let round_tripped = SerDe.serialize(deserialized);
+        eq(round_tripped, serialized);
+    },
+    "deserializing non-bas64 input throws an exception": function () {
+        let src = "Sphinx of black quartz, judge my vow";
+        let encoded = btoa(src);
+        let broken = src.substring(1);
+        try {
+            SerDe.deserialize(broken);
+        } catch {
+            return
+        }
+        fail("Expected an exception to be thrown");
+    },
+    "deserializing an invalid list throws an exception": function() {
+        try {
+            let invalid_list = [{"not": "the", "right": "keys"}];
+            SerDe.serialize(invalid_list);
+        } catch {
+            return
+        }
+        fail("Expected an exception to be thrown");
+    },
+    "deserializing something that's not a list throws an exception": function() {
+        try {
+            let invalid_object = "Alfalfa falls as a flask";
+            SerDe.serialize(invalid_object);
+        } catch {
+            return
+        }
+        fail("Expected an exeption to be thrown");
+    }
 });

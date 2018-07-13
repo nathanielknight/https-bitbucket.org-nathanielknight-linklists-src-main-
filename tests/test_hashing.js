5126a8324be13854f2e8874b49a8f6d591dd15f5
Nat Knight <nathaniel.ep@gmail.com>
2018-07-13 14:36:03 -0700

    Add tests for hashing

---
+++ tests/test_hashing.js
@@ -0,0 +1,21 @@
+
+tests({
+    "hash can be run without exceptions": function() {
+        Hash.signature(EXAMPLE_ITEMS);
+    },
+    "hash signature is different for different lists": function() {
+        let EXAMPLE_ITEMS = [
+            {id: "1", content: "First", done: true},
+            {id: "2", content: "Second", done: false},
+            {id: "3", content: "Third", done: false},
+        ];
+
+        let sig1 = Hash.signature(EXAMPLE_ITEMS);
+        EXAMPLE_ITEMS[0].content = "Something different";
+        let sig2 = Hash.signature(EXAMPLE_ITEMS);
+        if (sig1 !== sig2) {
+            fail("Signatures weren't different");
+        }
+    }
+
+})
